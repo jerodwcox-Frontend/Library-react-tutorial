e4ebfe7ad5a6fa85d442b2f22e9afeb6ed466465
jerodwcox-Frontend <dnldfn76@gmail.com>
2026-06-26 06:58:44 +0700

    fix eslint errors

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
-import { Link } from "react-router-dom";
 
 const Nav = ({ numberOfItems }) => {
   function openMenu() {
